c29abd224c89228acfd465ebcfd276da6aad0ddb
Koddom <koddom@gmail.com>
2021-10-11 11:10:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QGridLayout, QTableWidget, \
-    QTableWidgetItem, QLineEdit, QCheckBox, QDateEdit
-from PyQt5.QtCore import QSize, Qt, QRect, QDate
+    QTableWidgetItem, QLineEdit, QCheckBox, QDateEdit, QVBoxLayout, QMessageBox, QButtonGroup
+from PyQt5.QtCore import QSize, Qt, QDate
 import sys
 import maria
 
 
 class Window(QMainWindow):  # Стартовое окно с вводом логина и пароля
-    def __init__(self):
+    def __init__(self, auto_login: bool):
         super(Window, self).__init__()
-
         self.setWindowTitle('Авторизация')
         self.setGeometry(300, 300, 350, 300)
 
         self.username = QLineEdit(self)
-        self.username.setGeometry(QRect(50, 10, 200, 21))
         self.username.setPlaceholderText('Логин')
         self.username.setClearButtonEnabled(True)
         self.username.setObjectName('Name')
@@ -21,138 +19,77 @@ class Window(QMainWindow):  # Стартовое окно с вводом лог
         self.password = QLineEdit(self)
         self.password.setEchoMode(QLineEdit.Password)
         self.password.setPlaceholderText('Пароль')
-        self.password.setGeometry(QRect(50, 50, 200, 21))
 
         self.btn = QPushButton(self)
-        self.btn.move(10, 150)
         self.btn.setText('Вход')
         self.btn.setFixedWidth(100)
         self.btn.clicked.connect(self.open_phonebook)
 
         self.btn1 = QPushButton(self)
-        self.btn1.move(100, 150)
         self.btn1.setText('Регистрация')
         self.btn1.setFixedWidth(120)
         self.btn1.clicked.connect(self.registration)
 
         self.btn2 = QPushButton(self)
-        self.btn2.move(220, 150)
         self.btn2.setText('Отмена')
         self.btn2.setFixedWidth(100)
         self.btn2.clicked.connect(self.exit)
 
+        login, password = maria.autologin()
         self.remember_me = QCheckBox('Запомнить меня')
 
-        # вспомогательные строки чтобы каждый раз логин пароль не вводить
-        self.password.setText('pass')
-        self.username.setText('admin')
+        self.show_pass = QCheckBox('Показать пароль')
+        self.show_pass.toggled.connect(self.show_password)
+
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        v_layout = QVBoxLayout()
+        v_layout.addSpacing(1)
+        v_layout.addWidget(self.username)
+        v_layout.addWidget(self.password)
+        v_layout.addWidget(self.btn)
+        v_layout.addWidget(self.btn1)
+        v_layout.addWidget(self.btn2)
+        v_layout.addWidget(self.remember_me)
+        v_layout.addWidget(self.show_pass)
+        central_widget.setLayout(v_layout)
+
+        if login != '':
+            # self.remember_me.setChecked(True)
+            self.username.setText(login)
+            self.password.setText(password)
+        if auto_login:
+            self.open_phonebook()
+        else:
+            self.show()
+
+    def show_password(self):
+        if self.show_pass.isChecked():
+            self.password.setEchoMode(QLineEdit.Normal)
+        else:
+            self.password.setEchoMode(QLineEdit.Password)
 
     def open_phonebook(self):
-        if self.check_pass(self.username.text(), self.password.text()):
+        if maria.check_pass(self.username.text(), self.password.text(),self.remember_me.isChecked()):
             self.window = TableWindow()
             self.window.show()
             self.close()
         else:
-            print('пользователь с такими данными не найден')
-
-    def check_pass(self, login, password):
-        return maria.check_pass(login, password)
-
+            msg = QMessageBox()
+            msg.setWindowTitle("Ошибка авторизации")
+            msg.setText("пользователь с такими данными не найден")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
 
     def registration(self):
         self.window = RegistrationWindow()
         self.window.show()
         self.close()
 
-
     def exit(self):
         sys.exit()
-
-
-class TableWindow(QMainWindow):
-    def __init__(self):
-        super(TableWindow, self).__init__()
-
-        self.setMinimumSize(QSize(480, 600))
-        self.setWindowTitle('Телефонная книга')
-
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
-        self.grid_layout = QGridLayout()
-        central_widget.setLayout(self.grid_layout)
-        self.table = QTableWidget(self)
-        self.letters = 'аб'  # хранит буквы активной кнопки
-        self.create_table()  # создание таблицы с выборкой из БД
-
-        btn = QPushButton(self)
-        self.grid_layout.addWidget(btn, 0, 0)
-
-        self.create_alph_btn()
-
-    def create_alph_btn(self):
-        """ создаём кнопки бокового алфавита """
-        self.btn1 = QPushButton(self)
-        self.btn1.setText('АБ')
-        self.btn1.clicked.connect(lambda: self.update_letter(self.btn1.text()))
-
-        self.btn2 = QPushButton(self)
-        self.btn2.setText('ВГ')
-        self.btn2.clicked.connect(lambda: self.update_letter(self.btn2.text()))
-        self.btn2.move(0, 21)
-
-    def update_letter(self, letters):
-        self.letters = letters  # сначала меняем свойство
-        self.create_table()
-
-    def create_table(self) -> None:
-        """Формирует запрос в БД и заполняет таблицу данными выборки ФИО Телефон Дата"""
-        selected = maria.get_list_of_people(self.letters)  # отправляем запрос в БД для получения выборки по буквам
-        rows = selected.__len__()
-        columns = 4
-        self.table.setColumnCount(columns)
-        self.table.setRowCount(rows)
-
-        # Устанавливаем заголовки таблицы
-        self.table.setHorizontalHeaderLabels(['Имя', 'Телефон', 'День рождения', 'ID'])
-
-        # Устанавливаем выравнивание на заголовки
-        self.table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
-        self.table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignLeft)
-        self.table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
-
-        # Заполняем каждую строку согласно полученной выборке selected
-        for row in range(rows):
-            for column in range(columns):
-                self.table.setItem(row, column, QTableWidgetItem(str(selected[row][column])))
-
-        self.table.hideColumn(3)  # скрываем 4-ю колонку в которой хранится ID. Мы его будем использовать при изменении записи
-        # Делаем расайз колонок по содержимому
-        self.table.resizeColumnsToContents()
-        self.grid_layout.addWidget(self.table, 0, 1)  # Добавляем таблицу в сетку
-        self.table.cellChanged[int,int].connect(self.changed_cell)
-
-    def changed_cell(self, r, c):
-        print(r,c)
-        changed_data = self.table.item(r,c).text()  # получаем новые данные из колонки
-        user_id = self.table.item(r,3).text()  # user_id находится в скрытой колонке
-        if c == 0:  # если изменили имя
-            maria.update_name(user_id, changed_data)
-        elif c == 1:  # если поменяли телефон
-            maria.update_phone(user_id, changed_data)
-        elif c == 2:  # если изменили дату
-            maria.update_datebir(user_id, changed_data)
-
-        self.create_table()
-        # data = [self.table.item(r, column) for column in range(self.table.columnCount()+1)]  # получаем данные ряда в котором были произведены изменения
-
-
-
-    def change_table(self, letter):
-        '''заменяет содержимое таблицы'''
-        self.table.setItem(0, 0, QTableWidgetItem(letter))
-        self.table.setItem(0, 1, QTableWidgetItem(letter))
-        self.table.setItem(0, 2, QTableWidgetItem(letter))
 
 
 class RegistrationWindow(QMainWindow):
@@ -194,9 +131,7 @@ class RegistrationWindow(QMainWindow):
         self.password.setText('1234')
         self.password1.setText('1234')
 
-
     def ok(self):
-        print('Записать в БД')
         name = self.username.text()
         password = self.password.text()
         date = self.date.text().split('/')  # преобразуем дату в формат строки для DB
@@ -218,10 +153,127 @@ class RegistrationWindow(QMainWindow):
         self.close()
 
 
+class TableWindow(QMainWindow):
+    def __init__(self):
+        super(TableWindow, self).__init__()
+
+        self.setMinimumSize(QSize(480, 600))
+        self.setWindowTitle('Дни рождения на ближайшую неделю')
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        self.grid_layout = QGridLayout()
+        central_widget.setLayout(self.grid_layout)
+
+        self.letters = ''  # хранит буквы активной кнопки, если букв нет, будет показан список ДР
+        self.create_table()  # создание таблицы с выборкой из БД
+
+        btn = QPushButton(self)
+        btn.setText('exit')
+        btn.clicked.connect(self.logout)
+        self.grid_layout.addWidget(btn, 1, 0)
+
+        btn_delete = QPushButton(self)
+        btn_delete.setText('-')
+        # btn.clicked.connect(self.delete_user)
+        self.grid_layout.addWidget(btn_delete, 1, 1)
+
+        self.buttons = []
+        self.generate_alphas()
+
+        self.btn_grp = QButtonGroup()
+        self.btn_grp.setExclusive(True)
+        for button in self.buttons:
+            self.btn_grp.addButton(button)
+        self.btn_grp.buttonClicked.connect(self.letters_click)
+
+    def generate_alphas(self) -> None:
+        letters = ['АБ','ВГ','ДЕ','ЖЗИЙ','КЛ','МН','ОП','РС','ТУ','ФХ','ЦЧШЩ','ЪЫЬЭ','ЮЯ']
+        for i in letters:
+            btn = QPushButton(self)
+            btn.setText(i)
+            self.v_layout.addWidget(btn)
+            self.buttons.append(btn);
+
+    def letters_click(self, btn) -> None :
+        """меняет свойство активных букв"""
+        self.letters = btn.text()  # сначала меняем свойство
+        self.create_table()  # потом обновляем таблицу
+
+    def create_table(self) -> None:
+        """Формирует запрос в БД и заполняет таблицу данными выборки ФИО Телефон Дата"""
+        self.table = QTableWidget(self)
+        if self.letters != '':
+            selected = maria.get_list_of_people(self.letters)  # отправляем запрос в БД для получения выборки по буквам
+            self.setWindowTitle('Телефонная книга')
+        else:
+            selected = maria.get_list_of_birthday_people()
+        rows = selected.__len__()
+        columns = 4
+        self.table.setColumnCount(columns)
+        self.table.setRowCount(rows)
+
+        # Устанавливаем заголовки таблицы
+        self.table.setHorizontalHeaderLabels(['Имя', 'Телефон', 'День рождения', 'ID'])
+
+        # Устанавливаем выравнивание на заголовки
+        self.table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
+        self.table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignLeft)
+        self.table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
+
+        # Заполняем каждую строку согласно полученной выборке selected
+        for row in range(rows):
+            for column in range(columns):
+                self.table.setItem(row, column, QTableWidgetItem(str(selected[row][column])))
+
+        self.table.hideColumn(3)  # скрываем 4-ю колонку в которой хранится ID. Мы его будем использовать при изменении записи
+        # Делаем расайз колонок по содержимому
+        # self.table.resizeColumnsToContents()
+
+        self.v_layout = QVBoxLayout()
+        self.v_layout.setAlignment(Qt.AlignTop)
+
+        self.grid_layout.addLayout(self.v_layout, 0, 0)
+        self.grid_layout.addWidget(self.table, 0, 1)  # Добавляем таблицу в сетку
+        self.table.cellChanged[int,int].connect(self.changed_cell)  # если пользователь отредактировал запись
+
+    def changed_cell(self, r, c):
+        # print(r, c)
+        # changed_data = self.table.item(r, c).text()  # получаем новые данные из колонки
+        row_data = [str(self.table.item(r, column).text()) for column in range(self.table.columnCount())]  # получаем данные ряда в котором были произведены изменения
+        row_data = tuple(row_data)
+        user_id = self.table.item(r, 3).text()  # user_id находится в скрытой колонке
+        if maria.update_entry(row_data, c):
+            msg = QMessageBox()
+            msg.setWindowTitle("Ошибка изменения данных")
+            msg.setText("Запись успешно изменена")
+            # msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            print('')
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle("Ошибка изменения данных")
+            msg.setText("Такой пользователь уже существует")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+        self.create_table()
+
+    def logout(self):
+        self.window = Window(False)
+        self.close()
+
+    # def change_table(self, letter):
+    #     '''заменяет содержимое таблицы'''
+    #     self.table.setItem(0, 0, QTableWidgetItem(letter))
+    #     self.table.setItem(0, 1, QTableWidgetItem(letter))
+    #     self.table.setItem(0, 2, QTableWidgetItem(letter))
+
+
 def main():
+    maria.first_connection()  # проверяем подключение к базе
     app = QApplication(sys.argv)
-    window = Window()
-    window.show()
+    window = Window(False)
     sys.exit(app.exec_())
 
 
--- maria.py
+++ maria.py
@@ -2,43 +2,24 @@ import mariadb
 import sys
 
 
-FIRST_LAUNCH = True
+DATABASE = 'phonebook'
+CONN_PARAMS = None  # Определяется в функции first_connection()
 
 
 class CursorDB:
-    def __init__(self):
+    def __init__(self,):
         try:
-            # connection parameters
-            conn_params = {
-                "user": "egordmitriev",
-                "password": "",
-                "host": "localhost",
-                "database": ""
-            }
-            self.connection = mariadb.connect(**conn_params)
+            self.connection = mariadb.connect(**CONN_PARAMS)
         except mariadb.Error as e:
             print(f'Ошибка подключения к платформе MarinaDB: {e}')
             sys.exit(1)
 
         self.cursor = self.connection.cursor()
-        self.cursor.execute('USE phonebook')
-
-    def create_phonebook_data(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS phonebook_data('
-                            'UserID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,'
-                            'Name VARCHAR(150) NOT NULL,'
-                            'Password VARCHAR(100) NOT NULL,'
-                            'Phone VARCHAR(11),'
-                            'DateBir DATE NOT NULL,'
-                            'Remember_me BOOL NOT NULL DEFAULT 0)')
-        self.connection.commit()
-        self.cursor.close()
-        self.connection.close()
-        print('Таблица телефонных номеров создана')
+        self.cursor.execute('USE '+DATABASE)
 
 
 def get_list_of_people(letters) -> list:
-    """Отправляет запрос в БД и формирует выборку по буквам первым буквам имени LIKE в letters"""
+    """Отправляет запрос в БД и формирует выборку по первым буквам имени LIKE в letters"""
 
     # Сначала из всех букв letters получаем большие и маленькие, и формируем из них кортеж данных
     data = []
@@ -60,17 +41,42 @@ def get_list_of_people(letters) -> list:
     return selected
 
 
-def check_pass(login, password) -> bool:
-    """Проверят правильность пары логин-пароль"""
+def get_list_of_birthday_people() -> list:
+    """Возвращает список пользователей, чьё ДР выпадает на ближайшую неделю"""
+    sql = "SELECT Name,Phone,DateBir,UserID FROM phonebook_data WHERE DateBir BETWEEN CURRENT_DATE() AND ADDDATE(CURRENT_DATE(), INTERVAL ? DAY)"
+    data = (7,)
     gate = CursorDB()
-    gate.cursor.execute("SELECT Password FROM users WHERE Login=?", (login,))
+    gate.cursor.execute(sql, data)
+    selected = gate.cursor.fetchall()
+    gate.cursor.close()
+    gate.connection.close()
+
+    return selected
+
+
+def check_pass(login, password, remember_me) -> bool:
+    """Проверяет правильность пары логин-пароль"""
+    gate = CursorDB()
+    gate.cursor.execute("SELECT Password,UserID FROM phonebook_data WHERE Name=?", (login,))
     user_password = gate.cursor.next()
     if not user_password:  # если запись запроса пустая т.е пользователя с таким именем нет в базе
-        return False
+        access = False
     elif user_password[0] == password:  # если пароль в выборке совпадает с введённым пользователем
-        return True
-    else:
-        return False
+        access = True
+        if remember_me:
+            # Для начала удаляем все другие отметки Remember_me
+            gate.cursor.execute("UPDATE phonebook_data SET Remember_me=FALSE WHERE UserID IN( SELECT UserID FROM phonebook_data WHERE Remember_me=1)")
+            # записываем отметку запомнить меня для автоматического заполнения полей авторизации
+            gate.cursor.execute("UPDATE phonebook_data SET Remember_me=TRUE WHERE UserID=?", (user_password[1],))
+        else:
+            # или удаляем
+            gate.cursor.execute("UPDATE phonebook_data SET Remember_me=False WHERE UserID=?", (user_password[1],))
+        gate.connection.commit()
+    else:  # иначе введён неправильный пароль
+        access = False
+    gate.cursor.close()
+    gate.connection.close()
+    return access
 
 
 def add_new_entry_to_phonebook_data(name, password, date):
@@ -79,70 +85,143 @@ def add_new_entry_to_phonebook_data(name, password, date):
     gate.cursor.execute('SELECT * FROM phonebook_data WHERE Name=? AND Password=? AND DateBir=?', (name, password, date))
     if gate.cursor.next():
         print(f'Пользователь с именем {name}, и датой рождения {date} уже существует')
-        return False 
+        return False
     else:
         try:  # Добавляем новую запись
-            cursor.cursor.execute('INSERT INTO phonebook_data(Name,Password,DateBir) VALUES (?,?,?)', (name, password, date))
-            cursor.connection.commit()
+            gate.cursor.execute('INSERT INTO phonebook_data(Name,Password,DateBir) VALUES (?,?,?)', (name, password, date))
+            gate.connection.commit()
         except mariadb.Error as e:
             print(f'Не удалось добавить новую запись: {e}')
 
-    cursor.cursor.close()
-    cursor.connection.close()
+    gate.cursor.close()
+    gate.connection.close()
     return True  # Возвращаем истину если успешно добавили пользователя в БД
 
 
-def update_name(user_id, changed_data) -> None:
+def check_dublicate(cursor, row_data: tuple) -> bool:
+    """Возвращает True если пользователя в БД с row_data не обнаружено
+    row_data = (Name, Phone, DateBir)"""
+    sql = 'SELECT Name, Phone, DateBir FROM phonebook_data WHERE Name=? AND Phone=? AND DateBir=?'
+    cursor.execute(sql, row_data)
+    if cursor.next():  # Если уже существует запись с подобными параметрами
+        print('Нельзя изменить')
+        return False
+    else:
+        return True
+
+
+def update_entry(row_data, column) -> bool:
     """Меняет поле Name в таблице базы данных"""
     gate = CursorDB()
-    sql = 'UPDATE phonebook_data SET Name=? WHERE UserID=?'
-    data = (changed_data, user_id)
-    gate.cursor.execute(sql, data)
-    gate.connection.commit()
-    gate.cursor.close()
-    gate.connection.close()
-
-
-def update_phone(user_id, changed_data):
-    """Меняет поле Phone в таблице базы данных"""
-    gate = CursorDB()
-    sql = 'UPDATE phonebook_data SET Phone=? WHERE UserID=?'
-    data = (changed_data, user_id)
-    gate.cursor.execute(sql, data)
-    gate.connection.commit()
-    gate.cursor.close()
-    gate.connection.close()
-
-
-def update_datebir(user_id, changed_data):
-    gate = CursorDB()
-    try:
-        sql = 'UPDATE phonebook_data SET DateBir=? WHERE UserID=?'
-        data = (changed_data, user_id)
+    if check_dublicate(gate.cursor, row_data[:-1]): # если дубликатов не обнаружено
+        print('меняем запись')
+        if column == 0:  # Если изменили колонку с именем
+            field = 'Name'
+        elif column == 1:  # изменили телефон
+            field = 'Phone'
+        elif column == 2:  # значит изменили дату
+            field = 'DateBir'
+        sql = f'UPDATE phonebook_data SET {field}=? WHERE UserID=?'
+        data = (row_data[column], row_data[3])  # Получаем данные из нужной колонки, ID пользователя
         gate.cursor.execute(sql, data)
         gate.connection.commit()
-    except mariadb.Error as e:
-        print(f'Ошибка обновления даты: {e}')
+        commit = True
+    else:
+        commit = False
     gate.cursor.close()
     gate.connection.close()
+    return commit
 
 
-if FIRST_LAUNCH:
-    cursor = CursorDB()
-    cursor.create_phonebook_data()
-    FIRST_LAUNCH = False
+def autologin():
+    gate = CursorDB()
+    # Проверяем нет ли галочки запомнить меня для автоматического входа
+    gate.cursor.execute("SELECT Name,Password FROM phonebook_data WHERE UserID IN (SELECT UserID FROM auto_login)")
+    user_exists = gate.cursor.next()
+    if user_exists:
+        login = user_exists[0]
+        password = user_exists[1]
+    else:
+        login = ''
+        password = ''
+    gate.cursor.close()
 
-# cursor.execute('CREATE DATABASE IF NOT EXISTS phonebook')
-# cursor.execute('USE phonebook')
-# cursor.execute('CREATE TABLE IF NOT EXISTS users('
-#                'UserID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,'
-#                'Login VARCHAR(100) NOT NULL,'
-#                'Password VARCHAR(100) NOT NULL)')
-# try:
-#     cursor.execute("INSERT INTO users(UserID, Login, Password) VALUES (1,'admin','pass')")
-# except mariadb.Error as e:
-#     print('Duplicate entry' in str(e))
-#     print(f'ошибка создания пользователя admin: {e}')
-# connection.commit()
-# cursor.close()
-# connection.close()
+    return login, password
+
+
+def create_db(username_db, password):
+    """Функция должна сработать только при первом запуске скрипта и создать необходимую базу данных
+    с таблицей phonebook_data и пользователем admin pass"""
+    conn_params = {
+        'user': username_db,
+        'password': password,
+        'host': 'localhost',
+        'database': ''
+    }
+
+    data = []  # формируем информацию  из people.csv для передачи в аргументы к SQL запросу и записи в базу данных.
+    file = open('people.csv')
+    for line in file:
+        name_phone = line.split(',')
+        name_phone[1] = name_phone[1].strip()
+        data.append(tuple(name_phone))
+
+    connection = mariadb.connect(**conn_params)
+    cursor = connection.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS '+DATABASE)
+    cursor.execute('USE '+DATABASE)
+    print('База данных ' + DATABASE + ' создана')
+    cursor.execute('CREATE TABLE IF NOT EXISTS phonebook_data('  # создаём основную таблицу для хранения информации
+                        'UserID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,'
+                        'Name VARCHAR(150) NOT NULL,'
+                        'Password VARCHAR(100) NOT NULL DEFAULT "",'
+                        'Phone VARCHAR(11),'
+                        'DateBir DATE NOT NULL DEFAULT CURRENT_DATE(),'
+                        'Remember_me BOOL NOT NULL DEFAULT 0)')
+    cursor.execute("CREATE TABLE IF NOT EXISTS auto_login("  # вспомогательная таблица хранит пользователя с галочкой remember me
+                   "N INT NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+                   "UserID INT)")
+    connection.commit()
+    cursor.execute("INSERT INTO phonebook_data (UserID,Name,Password,Phone,DateBir) VALUES (1,'admin','pass','','0001-01-01')")
+    connection.commit()
+
+    cursor.executemany("INSERT INTO phonebook_data (Name,Phone) VALUES (?,?)", data)
+    cursor.execute("INSERT INTO auto_login (UserID)  VALUES (1)")
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print('Таблица телефонных номеров создана. Для доступа используйте\nлогин admin\nпароль pass')
+
+
+def first_connection():
+    """Функция для проверки подключения к базе данных"""
+    while True:  # Просим пользователя ввести логин пароль, пока не введёт верный. break строка в операторе try
+        print('Пытаемся подключится к базе данных. Введите логин и пароль от mariaDB')
+        username_db = 'egordmitriev'
+        password = ''
+
+        conn_params = {
+            'user': username_db,
+            'password': password,
+            'host': 'localhost',  # !!!!! <-- указывай все настройки необходимые для подключения к бд
+            'database': DATABASE
+        }
+        try:
+            connection = mariadb.connect(**conn_params)
+            connection.close()
+            break # если полдключение удалось, то можно выйти из цикла попыток
+        except mariadb.Error as e:
+            if e.errno == 1044:  # Если введены неверный логин, пароль
+                print('Доступ запрещён. Проверьте права пользователя', username_db)
+                # далее просим заново ввести логин и парольћ
+                continue
+            elif e.errno == 1049:  # если база данных не найдена
+                # создаём базу данных
+                create_db(username_db, password)
+                # connection = mariadb.connect(**conn_params)  # и подключаемся к базе данных
+                break
+
+    global CONN_PARAMS
+    CONN_PARAMS = conn_params
+
